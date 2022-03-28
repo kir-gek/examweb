251f703c021b10270349fdb13b9c224daed47a60
Kirill Gekgman <Kirokronos@gmail.com>
2022-03-28 14:35:54 +0300

    Создание карточки животного

---
+++ src/components/CardAnimal.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-card>
+		<v-img :src="`/` + animal.type + `_img.jpg`" height="250"></v-img>
+
+		<v-card-text>
+			<p class="title text-black" style="color: #000">{{ animal.name }}</p>
+			<p class="title text-black">Цвет: {{ animal.color }}</p>
+			<p class="title text-black">Вес:{{ animal.weight }}кг</p>
+			<p class="title text-black">Пол: {{ animal.sex }}</p>
+		</v-card-text>
+	</v-card>
+</template>
+
+<script>
+export default { props: ['animal'] }
+</script>
--- src/views/Animals.vue
+++ src/views/Animals.vue
@@ -1,3 +1,23 @@
 <template>
-	<h1>Животные</h1>
+    <div>
+        <h1>Животные</h1>
+
+        <CardAnimal 
+            :animal="{
+                type: 'cow',
+                name: 'бебра',
+                color: 'золотой',
+                weight: '50 кг',
+                sex: 'мужской'
+            }"
+        />
+    </div>
 </template>
+
+<script>
+import CardAnimal from '../components/CardAnimal.vue'
+
+export default {
+    components: { CardAnimal }
+}
+</script>
